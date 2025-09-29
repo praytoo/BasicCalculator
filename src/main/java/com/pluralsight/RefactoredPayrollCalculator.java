4b135d579958a918236026b3395b9b19a547fc7c
praytoo <praytoo52@icloud.com>
2025-09-29 11:10:10 -0700

    refactored payroll calculator

--- src/main/java/com/pluralsight/RefactoredPayrollCalculator.java
+++ src/main/java/com/pluralsight/RefactoredPayrollCalculator.java
@@ -6,26 +6,27 @@ public class RefactoredPayrollCalculator {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         String name = getName();
-        getHoursWorked();
-        getPayRate();
-        getGrossPay("Prince", 40, 120);
-        getGrossPay("Saint", 50, 150);
+        float hoursWorked = getHoursWorked();
+        float payrate = getPayRate();
+        getGrossPay(name, hoursWorked, payrate);
     }
         public static String getName() {
             System.out.println("What's your name?");
             String name = scanner.nextLine();
             return name;
         }
-        public static void getHoursWorked() {
+        public static float getHoursWorked() {
             System.out.println("How many hours have you worked?");
             float hoursworked = scanner.nextFloat();
             scanner.nextLine();
+            return hoursworked;
         }
 
-        public static void getPayRate() {
+        public static float getPayRate() {
             System.out.println("What is your pay rate per hour?");
             float payrate = scanner.nextFloat();
             scanner.nextLine();
+            return payrate;
         }
 
         public static void getGrossPay(String name, float hoursworked, float payrate) {
